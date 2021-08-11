0ed744b5902a404ba1c16b13743a79c29bcc199d
junmin-chang <chjm219@kookmin.ac.kr>
2021-08-11 17:24:38 +0900

    styling

--- components/navbar.js
+++ components/navbar.js
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { bubble as Menu } from "react-burger-menu";
 import Link from 'next/link'
 
 export default function SideBar(props) {
+
+
+    const [count, setCount] = useState({
+        algoCount: 0,
+        dataCount: 0
+    })
     return (
         // Pass on our props
         <Menu {...props}>
             <div>
                 <Link href="/posts/algorithms">
-                    <a className="bm-item">
-                        Algorithms
+                    <a>
+                        {`알고리즘(${count.algoCount})`}
                     </a>
 
                 </Link>
             </div>
             <div>
                 <Link href="/posts/data-structure">
-                    <a className="bm-item">
-                        자료구조
+                    <a>
+                        {`자료구조(${count.dataCount})`}
                     </a>
                 </Link>
             </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import '../styles/globals.css'
 import '../components/navbar.css'
-
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps}/>
+  return (
+
+        <Component {...pageProps}/>
+  )
 }
 
 export default MyApp
