bc63278f834bac5486975797a07a7b892998c08e
junmin-chang <chjm219@kookmin.ac.kr>
2021-08-12 15:53:43 +0900

    module error-1

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,6 +1,5 @@
 import {getSortedPostsData} from "../../lib/posts";
-const posts = process.env.NODE_ENV === 'production' ?
-    require('../../cache/data').posts : getSortedPostsData()
+const posts = process.env.NODE_ENV === 'production' ? require('../../cache/data').posts : getSortedPostsData()
 export default (req, res) => {
     const results = req.query.q ?
         posts.filter(post => post.title.toLowerCase().includes(req.query.q)) : []
--- scripts/cache.js
+++ scripts/cache.js
@@ -1,6 +1,6 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
+const fs = require('fs')
+const path = require('path')
+const matter = require('gray-matter')
 
 function getPosts() {
     const postDirectory = path.join(process.cwd(), 'posts')
