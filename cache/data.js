94a73369e69e227c506d8e12c771e69356fbeef9
junmin-chang <chjm219@kookmin.ac.kr>
2021-08-14 23:35:23 +0900

    cache post

--- cache/data.js
+++ cache/data.js
@@ -1,1 +1,1 @@
-export const posts = [{"id":"array","title":"std::array"},{"id":"first","title":"연속된 자료 구조와 연결된 자료구조"},{"id":"queue","title":"Queue"},{"id":"stack","title":"스택(Stack)"}]
+export const posts = [{"id":"array","title":"std::array"},{"id":"first","title":"연속된 자료 구조와 연결된 자료구조"},{"id":"linked-list","title":"링크드 리스트(Linked List)"},{"id":"queue","title":"Queue"},{"id":"stack","title":"스택(Stack)"}]
